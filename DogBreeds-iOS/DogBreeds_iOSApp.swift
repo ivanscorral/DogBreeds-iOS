91c4d92d7b875dd932a5d2373fe16776d2b6f7ad
Ivan Sanchez Corral <ramon.sanchez68@gmail.com>
2025-06-02 15:25:37 +0200

    Initial Commit

---
+++ DogBreeds-iOS/DogBreeds_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreeds_iOSApp.swift
+//  DogBreeds-iOS
+//
+//  Created by Ivan Sanchez Corral on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DogBreeds_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
